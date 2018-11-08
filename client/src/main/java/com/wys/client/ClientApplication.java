00ada6d98e5f48aa86691b2bcea34a657d47f577
-001 <1219288694@qq.com>
2018-11-08 17:45:54 +0800

    service-ribbon 负载均衡

--- client/src/main/java/com/wys/client/ClientApplication.java
+++ client/src/main/java/com/wys/client/ClientApplication.java
@@ -1,11 +1,14 @@
 package com.wys.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,6 +20,9 @@ public class ClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ClientApplication.class, args);
     }
+
+
+
 }
 
 @RestController
@@ -25,9 +31,17 @@ class ClientController {
     @Autowired
     private DiscoveryClient discoveryClient;
 
+    @Value("${server.port}")
+    String port;
+
     @GetMapping(value = "/test")
     public List<String> test() {
         return this.discoveryClient.getServices();
+    }
+
+    @RequestMapping("/hi")
+    public String home(String name) {
+        return "hi " + name + " ,i am from port:" + port;
     }
 
 }
--- client2/src/main/java/com/wys/client2/Client2Application.java
+++ client2/src/main/java/com/wys/client2/Client2Application.java
@@ -1,11 +1,14 @@
 package com.wys.client2;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,6 +20,8 @@ public class Client2Application {
     public static void main(String[] args) {
         SpringApplication.run(Client2Application.class, args);
     }
+
+
 }
 
 @RestController
@@ -30,4 +35,11 @@ class ClientController {
         return this.discoveryClient.getServices();
     }
 
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(String name) {
+        return "hi " + name + " ,i am from port:" + port;
+    }
 }
