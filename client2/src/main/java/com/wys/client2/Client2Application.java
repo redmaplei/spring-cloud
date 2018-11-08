15f0989370f83c89c05642cc97506d76cce03892
-001 <1219288694@qq.com>
2018-11-08 15:32:11 +0800

    cloud-init

---
+++ client2/src/main/java/com/wys/client2/Client2Application.java
@@ -0,0 +1,33 @@
+package com.wys.client2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Client2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Client2Application.class, args);
+    }
+}
+
+@RestController
+class ClientController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping(value = "/test")
+    public List<String> test() {
+        return this.discoveryClient.getServices();
+    }
+
+}
