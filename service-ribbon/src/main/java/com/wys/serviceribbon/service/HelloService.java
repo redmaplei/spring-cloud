74730aeaee854caa3770f990f8ad252ff232e819
-001 <1219288694@qq.com>
2018-11-09 16:09:39 +0800

    ribbon使用短路器 hystrix

--- service-ribbon/src/main/java/com/wys/serviceribbon/service/HelloService.java
+++ service-ribbon/src/main/java/com/wys/serviceribbon/service/HelloService.java
@@ -1,5 +1,6 @@
 package com.wys.serviceribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,11 +14,17 @@ public class HelloService {
     /**
      * ribbon 负载均衡
      * 自动去找client服务
+     * 加入段路由器模式
      * @param name
      * @return
      */
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
         return restTemplate.getForObject("http://client/hi?name="+name,String.class);
+    }
+
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
     }
 
 }
